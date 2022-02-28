4c216a028b66f7b3379750dfb721528c4e3ed30c
mendim <menahemmizrahy99@gmail.com>
2022-02-28 09:12:06 +0200

    routing...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Routs, Route } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <Routs>
+      <Route path="/" />
+      <Route path="/new-game">
+        <Route path="/questions" />
+        <Route path="/finish" />
+      </Route>
+      <Route path="/game">
+        <Route path="/questions" />
+        <Route path="/wait" />
+        <Route path="/finish" />
+      </Route>
+      <Route path="/admin">
+        <Route path="/game">
+          <Route path="/questions" />
+          <Route path="/wait" />
+          <Route path="/finish" />
+        </Route>
+      </Route>
+    </Routs>
+  );
 }
 
 export default App;
