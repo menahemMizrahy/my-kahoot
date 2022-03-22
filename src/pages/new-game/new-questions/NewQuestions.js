08bc5e2a28215d5729ee20fe621eff772290b74e
mendim <menahemmizrahy99@gmail.com>
2022-03-22 14:37:25 +0200

    f

--- src/pages/new-game/new-questions/NewQuestions.js
+++ src/pages/new-game/new-questions/NewQuestions.js
@@ -1,15 +1,33 @@
 import { Grid } from "@mui/material";
 
 const NewQuestions = () => {
+  let questionsCounter = 1;
   return (
     <Grid container>
-      <Grid item xs={1}>
-        left
+      <Grid
+        item
+        xs={2}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <div
+          style={{
+            borderRadius: "50%",
+            backgroundColor: "grey",
+            height: "6em",
+            width: "6em",
+          }}
+        >
+          {`# ${questionsCounter}`}
+        </div>
       </Grid>
-      <Grid item xs={10}>
+      <Grid item xs={8}>
         center
       </Grid>
-      <Grid item xs={1}>
+      <Grid item xs={2}>
         right
       </Grid>
     </Grid>
