73a8cf571fa19d5025cf9002e64bb1b3418d6957
mendi <menahemmizrahy99@gmail.com>
2022-04-12 00:58:20 -1000

    j

--- src/pages/new-game/questions/components/BooleanQuestion.js
+++ src/pages/new-game/questions/components/BooleanQuestion.js
@@ -1,57 +1,16 @@
 import { useState } from "react";
 import { Button, Typography } from "@mui/material";
+import useBooleanQuestion from "./useBooleanQuestion";
 
-const initialAnswers = {
-  trueAnswer: false,
-  falseAnswer: false,
-};
-
-const BooleanQuestion = ({
-  question,
-  isOpenQuestion,
-  onSubmit,
-  navigate,
-  resetQuestion,
-}) => {
-  const [checked, setChecked] = useState(initialAnswers);
-  const [answerError, setAnswerError] = useState(false);
-
-  const answerCheckedHandler = (event) => {
-    setAnswerError(false);
-    setChecked(() => {
-      return { ...initialAnswers, [event.target.value]: true };
-    });
-  };
-
-  const validateFileds = () => {
-    question.onBlur();
-    setAnswerError(
-      checked.falseAnswer === false && checked.trueAnswer === false
-    );
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-    if (answerError || question.error) {
-      return;
-    }
-    onSubmit({
-      question: question.value,
-      isOpenQuestion,
-      correctAnswer: checked.trueAnswer,
-    });
-    resetQuestion();
-    setChecked(initialAnswers);
-  };
-
-  const finishHandler = () => {
-    if (!question.value) {
-      question.onBlur();
-      return;
-    }
-    navigate("../finish");
-  };
-
+const BooleanQuestion = (props) => {
+  const {
+    submitHandler,
+    checked,
+    answerCheckedHandler,
+    answerError,
+    validateFileds,
+    finishHandler,
+  } = useBooleanQuestion(props);
   return (
     <form onSubmit={submitHandler}>
       <Typography variant="h3" sx={{ m: "2rem" }}>
---
+++ src/pages/new-game/questions/components/useBooleanQuestion.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+const useBooleanQuestion = ({
+  question,
+  isOpenQuestion,
+  onSubmit,
+  navigate,
+  resetQuestion,
+}) => {
+  const initialAnswers = {
+    trueAnswer: false,
+    falseAnswer: false,
+  };
+  const [checked, setChecked] = useState(initialAnswers);
+  const [answerError, setAnswerError] = useState(false);
+
+  const answerCheckedHandler = (event) => {
+    setAnswerError(false);
+    setChecked(() => {
+      return { ...initialAnswers, [event.target.value]: true };
+    });
+  };
+
+  const validateFileds = () => {
+    question.onBlur();
+    setAnswerError(
+      checked.falseAnswer === false && checked.trueAnswer === false
+    );
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (answerError || question.error) {
+      return;
+    }
+    onSubmit({
+      question: question.value,
+      isOpenQuestion,
+      correctAnswer: checked.trueAnswer,
+    });
+    resetQuestion();
+    setChecked(initialAnswers);
+  };
+
+  const finishHandler = () => {
+    if (!question.value) {
+      question.onBlur();
+      return;
+    }
+    navigate("../finish");
+  };
+  return {
+    submitHandler,
+    checked,
+    answerCheckedHandler,
+    answerError,
+    validateFileds,
+    finishHandler,
+  };
+};
+export default useBooleanQuestion;
